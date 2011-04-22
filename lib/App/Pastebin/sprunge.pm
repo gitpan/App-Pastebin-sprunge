57d259c2c7b1856ebb74dbe2acbe7bd3b5ebea98
Mike Doherty <doherty@cpan.org>
2011-04-22 00:59:59 +0000

    Import of DOHERTY/App-Pastebin-sprunge-0.005 from CPAN.
    gitpan-cpan-distribution: App-Pastebin-sprunge
    gitpan-cpan-version:      0.005
    gitpan-cpan-path:         DOHERTY/App-Pastebin-sprunge-0.005.tar.gz
    gitpan-cpan-author:       DOHERTY
    gitpan-cpan-maturity:     released

--- lib/App/Pastebin/sprunge.pm
+++ lib/App/Pastebin/sprunge.pm
@@ -1,70 +1,55 @@
-use strict;
-use warnings;
 package App::Pastebin::sprunge;
-BEGIN {
-  $App::Pastebin::sprunge::VERSION = '0.004';
-}
-# ABSTRACT: application class for pasting to and reading from sprunge.us
+use perl5i::2;
+# ABSTRACT: application for pasting to and reading from sprunge.us
+our $VERSION = '0.005'; # VERSION
 
 
-sub new {
-    my $class = shift;
-    my $name  = shift;
-
-    my $self = { name => $name };
-    bless($self, $class);
-    return $self;
-}
-
-
-sub run {
-    my $self = shift;
-    my $opts = shift;
-    my $args = shift;
-
-    if ($opts->{'version'}) {
-        require File::Basename;
-        my $this = File::Basename::basename($0);
-        my $this_ver = App::Pastebin::sprunge->VERSION();
-        print "$this version $this_ver\n" and exit;
-    }
-
-    if (! scalar @$args) {  # WRITE
-        require WWW::Pastebin::Sprunge::Create;
-        require File::Slurp;
-        my $writer = WWW::Pastebin::Sprunge::Create->new();
-        my $text = File::Slurp::slurp(\*STDIN);
-        $writer->paste($text, lang => $opts->{'lang'})
-            or warn "Paste failed: " . $writer->error() . "\n"
-            and exit 1;
-        print "$writer\n";
-    }
-    else {                  # READ
-        my $paste_id = shift @ARGV;
+method new ($class:) {
+    my $self;
+    if (@ARGV) {                # READ
+        $self->{paste_id} = shift @ARGV;
         require WWW::Pastebin::Sprunge::Retrieve;
-        my $reader = WWW::Pastebin::Sprunge::Retrieve->new();
-        $reader->retrieve($paste_id)
-            or warn "Reading paste $paste_id failed: " . $reader->error() . "\n"
-            and exit 1;
-        print "$reader\n";
+        $self->{reader} = WWW::Pastebin::Sprunge::Retrieve->new();
     }
+    else {                      # WRITE
+        require WWW::Pastebin::Sprunge::Create;
+        $self->{writer} = WWW::Pastebin::Sprunge::Create->new();
+    }
+    return bless $self, $class;
 }
 
-1;
 
+method run($lang) {
+    if ($self->{paste_id}) {    # READ
+        $self->{reader}->retrieve($self->{paste_id})
+            or warn "Reading paste $self->{paste_id} failed: "
+            . $self->{reader}->error() . "\n"
+            and exit 1;
+        say $self->{reader};
+    }
+    else {                      # WRITE
+        my $text = do { local $/; <STDIN> };
+        $self->{writer}->paste($text, lang => $lang)
+            or warn 'Paste failed: '
+            . $self->{writer}->error() . "\n"
+            and exit 1;
+        say $self->{writer};
+    }
+    return;
+}
 
-
+__END__
 =pod
 
 =encoding utf-8
 
 =head1 NAME
 
-App::Pastebin::sprunge - application class for pasting to and reading from sprunge.us
+App::Pastebin::sprunge - application for pasting to and reading from sprunge.us
 
 =head1 VERSION
 
-version 0.004
+version 0.005
 
 =head1 SYNOPSIS
 
@@ -74,50 +59,27 @@ version 0.004
 
 =head1 DESCRIPTION
 
-B<App::Pastebin::sprunge> provides an application interface to L<WWW::Pastebin::Sprunge::Create>
-and L<WWW::Pastebin::Sprunge::Retrieve>, which allow creating and retrieving
-pastes on the L<http://sprunge.us> pastebin.
+B<App::Pastebin::sprunge> provides an application interface to
+L<WWW::Pastebin::Sprunge::Create> and L<WWW::Pastebin::Sprunge::Retrieve>,
+which allow creating and retrieving pastes on the L<http://sprunge.us> pastebin.
 
-This distribution provides an executable L<sprunge>, which provides a simple command-line
-client for L<http://sprunge.us> using this library.
+This distribution provides an executable L<sprunge>, which provides a simple
+command-line client for L<http://sprunge.us> using this library.
 
 =head1 METHODS
 
 =head2 new
 
-B<new()> is the constructor, and creates an application object. Takes no parameters.
+B<new()> is the constructor, and creates an application object. Takes no
+parameters.
 
 =head2 run
 
 B<run()> runs the application.
 
-Takes two optional parameters:
-
-=head3 Options
-
-The first is an options hash reference:
-
-=over 4
-
-=item *
-
-If I<version> is present, the application will print out version information and exit.
-
-=item *
-
 If I<lang> is present, the application will append this to the returned URI.
-L<http://sprunge.us> uses L<Pygments|http://pygments.org> for syntax highlighting.
-In the future, there might be a check that the supplied syntax highlighter is one
-supported by Pygments.
-
-=back
-
-=head3 Arguments
-
-The second parameter is an array reference. Only the first element is used. It
-is either the URI to a paste to read, or the ID portion of the URI (the query
-string). If this is present, the application will read and print out the specified
-paste.
+L<http://sprunge.us> uses L<Pygments|http://pygments.org> for syntax
+highlighting.
 
 =head1 AVAILABILITY
 
@@ -125,17 +87,22 @@ The latest version of this module is available from the Comprehensive Perl
 Archive Network (CPAN). Visit L<http://www.perl.com/CPAN/> to find a CPAN
 site near you, or see L<http://search.cpan.org/dist/App-Pastebin-sprunge/>.
 
-The development version lives at L<http://github.com/doherty/App-Pastebin-sprunge.git>
+The development version lives at L<http://github.com/doherty/App-Pastebin-sprunge>
 and may be cloned from L<git://github.com/doherty/App-Pastebin-sprunge.git>.
 Instead of sending patches, please fork this project using the standard
 git and github infrastructure.
+
+=head1 SOURCE
+
+The development version is on github at L<http://github.com/doherty/App-Pastebin-sprunge>
+and may be cloned from L<git://github.com/doherty/App-Pastebin-sprunge.git>
 
 =head1 BUGS AND LIMITATIONS
 
 No bugs have been reported.
 
 Please report any bugs or feature requests through the web interface at
-L<http://github.com/doherty/App-Pastebin-sprunge/issues>.
+L<https://github.com/doherty/App-Pastebin-sprunge/issues>.
 
 =head1 AUTHOR
 
@@ -150,5 +117,3 @@ the same terms as the Perl 5 programming language system itself.
 
 =cut
 
-
-__END__
